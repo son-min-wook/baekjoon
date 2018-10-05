97adbeffd3563e516fecc01a7ee5954e558298da
huh <desepticon@naver.com>
2018-10-05 13:31:47 +0900

    Add files via upload

---
+++ 1001/1001/1001.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a = -1, b = -1;
+	do {
+		scanf("%d %d", &a, &b);
+	} while (0 >= a || 10 <= a && 0 >= b || 10 <= b);
+	printf("%d\n", a - b);
+	return 0;
+}
